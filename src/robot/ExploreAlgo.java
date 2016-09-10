b661ee8cb9adbc628f03165e78d5d4a4464e3015
Allen-Shao <shaoyiyang312@hotmail.com>
2016-09-11 01:48:49 +0800

    create Exploration Algorithm Class

---
+++ src/robot/ExploreAlgo.java
@@ -0,0 +1,26 @@
+package robot;
+
+import map.Map;
+import map.MapGrid;
+import map.MapConstants;
+import robot.Robot;
+
+import java.util.*;
+
+public class ExploreAlgo{
+
+	private Map trueMap;
+	private Map knownMap;
+	private Robot expRobot;
+
+	public ExploreAlgo(Map tMp, Robot r){
+		this.trueMap = tMp;
+		this.expRobot = r;
+
+		this.knownMap = new Map();
+		knownMap.addBorder();
+
+	}
+
+
+}
