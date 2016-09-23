1e6a5b5273c89bf99c41d30805dba0664add1a5d
Allen-Shao <shaoyiyang312@hotmail.com>
2016-09-23 09:51:05 +0800

    add track files

---
+++ src/robot/Robot.java
@@ -0,0 +1,50 @@
+package robot;
+
+import map.*;
+import java.util.*;
+
+
+
+public class Robot {
+	private MapGrid position;
+	private int heading;      //Direction of the robot 1:right, 2:down, 3:left, 4:up
+	
+	private ArrayList<Sensor> sensors = new ArrayList<Sensor>();
+	
+	
+	public Robot(MapGrid pos, int h){
+		this.position = pos;
+		this.heading = h;
+	}
+
+	public MapGrid getPosition(){
+		return this.position;
+	}
+
+	public int getHeading(){
+		return this.heading;
+	}
+
+	public ArrayList<Sensor> getSensors(){
+		return this.sensors;
+	}
+
+	public void setPosition(MapGrid pos){
+		this.position = pos;
+	}
+
+	public void setHeading(int h){
+		this.heading = h;
+	}
+
+	public void addSensor(Sensor s){
+		sensors.add(s);
+	}
+
+	public void removeSensor(Sensor s){
+		sensors.remove(s);
+	}
+	
+	
+	
+}
---
+++ src/robot/RobotConstants.java
@@ -0,0 +1,11 @@
+package robot;
+
+public final class RobotConstants {
+
+
+	public static final double INFINITY = 100000.0;
+
+	public static final double MOVE_COST = 1.0;
+	public static final double TURN_COST = 10.0;
+
+}
