5c0741faf6f62f579e184791722f80d60370279d
EYiLin10 <eleanore_yilin@yahoo.com>
2016-09-26 14:45:00 +0800

    moves codes into functions

--- src/simulator/Mainframe.java
+++ src/simulator/Mainframe.java
@@ -103,6 +103,7 @@ public class Mainframe extends JFrame {
 		btnReset.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// btnShortestPath.setEnabled(true);
+				trueMap.resetMap();
 			}
 		});
 
@@ -176,6 +177,93 @@ public class Mainframe extends JFrame {
 		/*
 		 * Grid Buttons
 		 */
+		createGridButtons();
+		setStartPoint();
+		setEndPoint();
+
+		/*
+		 * Robot Circle
+		 */
+		// r.paint(null);
+
+		/*
+		 * Text Area indicating Color Representation
+		 */
+		createText();
+
+	}
+
+	JButton getGridButton(int r, int c) {
+		int index = r * N + c;
+		return list.get(index);
+	}
+
+	private JButton createGridButton(final int row, final int col) {
+		final JButton b = new JButton("r" + row + ",c" + col);
+		b.setBackground(Color.WHITE);
+		b.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				JButton gb = Mainframe.this.getGridButton(row, col);
+				System.out.println("r" + row + ",c" + col + " " + (b == gb) + " " + (b.equals(gb)));
+			}
+
+		});
+
+		b.addMouseListener(new MouseAdapter() {
+
+			public void mouseClicked(MouseEvent arg0) {
+				// when clicked, if true set false, else set true
+				JButton btn = Mainframe.this.getGridButton(row, col);
+				if (btn.isEnabled() == true) {
+					btn.setEnabled(false);
+					if (btn.getBackground() == Color.WHITE) {
+						btn.setBackground(Color.BLACK);
+						newMap.addObstacle(row, col);
+					}
+				} else {
+					btn.setEnabled(true);
+					if (btn.getBackground() == Color.BLACK) {
+						btn.setBackground(Color.WHITE);
+						// to add a remove obstacle method
+					}
+				}
+			}
+		});
+		return b;
+	}
+
+	public void updateBar_exp(int newValue) {
+		progressBar_exp.setValue(newValue);
+	}
+
+	public void updateBar_sp(int newValue) {
+		progressBar_sp.setValue(newValue);
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.setColor(new Color(0, 255, 0));
+		g.fillOval(100, 100, 50, 50);
+	}
+
+	public void setStartPoint() {
+		for (int i = 17; i < M; i++) {
+			for (int j = 0; j < 3; j++) {
+				Mainframe.this.getGridButton(i, j).setBackground(Color.YELLOW);
+			}
+		}
+	}
+
+	public void setEndPoint() {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 12; j < N; j++) {
+				Mainframe.this.getGridButton(i, j).setBackground(Color.YELLOW);
+			}
+		}
+	}
+
+	public void createGridButtons() {
 		JPanel p = new JPanel();
 		p.setBounds(50, 53, 581, 561);
 		contentPane.add(p);
@@ -186,36 +274,12 @@ public class Mainframe extends JFrame {
 				JButton gb = createGridButton(row, col);
 				list.add(gb);
 				p.add(gb);
-				gb.addMouseListener(new MouseAdapter() {
-					@Override // Manually add obstacles
-					public void mouseClicked(MouseEvent arg0) {
-						newMap.addObstacle(row, col);
-					}
-				});
 			}
 		}
 
-		// Set Start Point
-		for (int i = 17; i < M; i++) {
-			for (int j = 0; j < 3; j++) {
-				Mainframe.this.getGridButton(i, j).setBackground(Color.YELLOW);
-			}
-		}
-		// Set End Point
-		for (int i = 0; i < 3; i++) {
-			for (int j = 12; j < N; j++) {
-				Mainframe.this.getGridButton(i, j).setBackground(Color.YELLOW);
-			}
-		}
+	}
 
-		/*
-		 * Robot Circle
-		 */
-		// r.paint(null);
-
-		/*
-		 * Text Area indicating Color Representation
-		 */
+	public void createText() {
 		JTextArea txtrExplored = new JTextArea();
 		txtrExplored.setFont(new Font("Britannic Bold", Font.PLAIN, 13));
 		txtrExplored.setForeground(Color.WHITE);
@@ -246,57 +310,6 @@ public class Mainframe extends JFrame {
 		txtrRobot.setText("Robot");
 		txtrRobot.setBounds(756, 200, 67, 24);
 		contentPane.add(txtrRobot);
-
-	}
-
-	JButton getGridButton(int r, int c) {
-		int index = r * N + c;
-		return list.get(index);
-	}
-
-	private JButton createGridButton(final int row, final int col) {
-		final JButton b = new JButton("r" + row + ",c" + col);
-		b.setBackground(Color.WHITE);
-		b.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-				JButton gb = Mainframe.this.getGridButton(row, col);
-				System.out.println("r" + row + ",c" + col + " " + (b == gb) + " " + (b.equals(gb)));
-			}
-
-		});
-
-		b.addMouseListener(new MouseAdapter() {
-
-			public void mouseClicked(MouseEvent arg0) {
-				// when clicked, if true set false, else set true
-				JButton btn = Mainframe.this.getGridButton(row, col);
-				if (btn.isEnabled() == true) {
-					btn.setEnabled(false);
-					if (btn.getBackground() == Color.WHITE)
-						btn.setBackground(Color.BLACK);
-				} else {
-					btn.setEnabled(true);
-					if (btn.getBackground() == Color.BLACK)
-						btn.setBackground(Color.WHITE);
-				}
-			}
-		});
-		return b;
-	}
-
-	public void updateBar_exp(int newValue) {
-		progressBar_exp.setValue(newValue);
-	}
-
-	public void updateBar_sp(int newValue) {
-		progressBar_sp.setValue(newValue);
-	}
-
-	public void paint(Graphics g) {
-		super.paint(g);
-		g.setColor(new Color(0, 255, 0));
-		g.fillOval(100, 100, 50, 50);
 
 	}
 }
